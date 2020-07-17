d2a73488abb0798c2664760b5c157fe798f0e1fc
Rubayz <rubayzaf2007@gmail.com>
2020-07-17 18:49:10 +0600

    middle construction

--- assignment.js
+++ assignment.js
@@ -4,3 +4,17 @@ function feetToMile(feet){
     var Mile = feet / 5280;
     return Mile;
 }
+
+
+
+// Second Part of the assignment.
+
+function woodCalculator(chair, table, bed){
+    
+}
+
+// Last Part of the Assignment
+
+function tinyFriend(friends){
+    var friends = ['Jhankar', 'Mahbub', 'Moly', 'Sadia', 'Soumrik', 'Rumaisa', 'Mira'];
+}
