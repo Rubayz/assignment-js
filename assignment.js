94818eac284940eeeb4e7fcef73481b934f11182
Rubayz <rubayzaf2007@gmail.com>
2020-07-18 13:32:18 +0600

    half finished

--- assignment.js
+++ assignment.js
@@ -13,3 +13,20 @@ var faisalFeet = feetToMile(persons[1]);
 console.log(faisalFeet);
 var sobitaFeet = feetToMile(persons[2]);
 console.log(sobitaFeet);
+
+// Second Part of the assignment
+// Just for note:
+// 1 cubicFeet = 1 chair.
+// 3 cubicFeet = 1 table.
+// 5 cubicFeet = 1 bed.
+function woodCalculator(chair, table, bed){
+    var amountOfChair = chair * 1;
+    var amountOfTable = table * 3;
+    var amountOfBed = bed * 5;
+    var total = amountOfChair + amountOfTable + amountOfBed;
+    return total;
+}
+var amounts = woodCalculator(14, 5, 12);
+console.log(amounts);
+
+// Third part of the assignment
