b69408f16395cc71b72253cd33370e85fc027a3b
Rubayz <rubayzaf2007@gmail.com>
2020-07-23 14:11:36 +0600

    finished badly

--- assignment.js
+++ assignment.js
@@ -30,14 +30,20 @@ var amounts = woodCalculator(14, 5, 12);
 console.log(amounts);
 
 // Third part of the assignment
-function brickCalculator(){
 
+function brickCalculator(buildingFloor){
+    var firstFloors = [1,2,3,4,5,6,7,8,9,10];
+    var secondFloors = [11,12,13,14,15,16,17,18,19,20];
+    var thirdFloors = [21,22,23,24,25,26,27,28,29,30];
+    if(buildingFloor > 10){
+        firstFloors = 15 * 1000;
+        secondFloors = 12 * 1000;
+        thirdFloors  = 10 * 1000;
+    }
 }
-
-
-
+var floor = brickCalculator(7);
+console.log(floor);
 // Last Part of the assignment.
-function tinyFriend(tinyFriend){
-    var friends = ['Rafik', 'Karim', 'Salam', 'Borkot', 'Jabbar', 'Doly'];
+function tinyFriend(){
     
 }
